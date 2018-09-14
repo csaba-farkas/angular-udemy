0c19aa24ae327d5aa1f7193d0c9a95b6741eae39
Csaba Farkas <csabafarkas17@gmail.com>
2018-09-14 22:48:08 +0100

    Binding to input events

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -8,12 +8,23 @@ import { Component, OnInit } from '@angular/core';
 export class ServersComponent implements OnInit {
 
   allowNewServer = false;
+  serverCreationStatus = 'No servers were created!';
+  serverName = '';
 
   constructor() {
       setTimeout(() => { this.allowNewServer = true; },2000);
    }
 
   ngOnInit() {
+  }
+
+  onCreateServer() {
+      this.serverCreationStatus = 'Server was created';
+  }
+
+  onUpdateServerName(event: Event) {
+    console.log((<HTMLInputElement> event.target).value);
+    this.serverName = (<HTMLInputElement> event.target).value;
   }
 
 }
