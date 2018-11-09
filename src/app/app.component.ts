ceda1fd8f77b9397239a8799eede4686f7c57f4e
lastminute84 <csabafarkas17@gmail.com>
2018-11-09 23:03:39 +0000

    Submit form and use the data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,15 @@ export class AppComponent {
     answer: string;
     defaultQuestion = 'pet';
     genders = ['male', 'female'];
+    user = {
+        username: '',
+        email: '',
+        secretQuestion: '',
+        answer: '',
+        gender: ''
+    }
 
+    submitted = false;
     suggestUserName() {
         const suggestedName = 'Superuser';
 
@@ -36,7 +44,12 @@ export class AppComponent {
     }
 
     onSubmit(form: NgForm) {
-        console.log(form);
+        this.submitted = true;
+        this.user.username = this.form.value.userData.username;
+        this.user.email = this.form.value.userData.email;
+        this.user.secretQuestion = this.form.value.secret;
+        this.user.answer = this.form.value.questionAnswer;
+        this.user.gender = this.form.value.gender;
     }
 
     // alternative option is using the local reference grabbed by ViewChild
