9a9e6c6102478bae30fcd0830322f2b2d00965d5
lastminute84 <csabafarkas17@gmail.com>
2018-11-09 22:56:34 +0000

    Patching the form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,8 +11,28 @@ export class AppComponent {
     answer: string;
     defaultQuestion = 'pet';
     genders = ['male', 'female'];
+
     suggestUserName() {
         const suggestedName = 'Superuser';
+
+        // this is one approach to set a value in the form but it's long and may reset all other changes
+        // this.form.setValue({
+        //     userData: {
+        //         username: suggestedName,
+        //         email: ''
+        //     },
+        //     secret: 'pet',
+        //     questionAnswer: '',
+        //     gender: 'male'
+        // }
+        // )
+
+        // the better approach is to use patchValue because it overrides only parts of the form
+        this.form.form.patchValue({
+            userData: {
+                username: suggestedName
+            }
+        })
     }
 
     onSubmit(form: NgForm) {
