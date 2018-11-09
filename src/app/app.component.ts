79b48e035aa0298e883e81f696b4879c56b7682f
lastminute84 <csabafarkas17@gmail.com>
2018-11-09 22:32:28 +0000

    One-way binding of ngModel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,8 @@ import { NgForm } from '@angular/forms';
 })
 export class AppComponent {
     @ViewChild('f') form: NgForm;
+
+    defaultQuestion = 'pet';
     suggestUserName() {
         const suggestedName = 'Superuser';
     }
