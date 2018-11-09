c9ec60fe3c8a194ea8fa21c7c3ed709b8ede867d
lastminute84 <csabafarkas17@gmail.com>
2018-11-09 23:54:49 +0000

    Complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,58 +7,21 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-    @ViewChild('f') form: NgForm;
-    answer: string;
-    defaultQuestion = 'pet';
-    genders = ['male', 'female'];
-    user = {
-        username: '',
-        email: '',
-        secretQuestion: '',
-        answer: '',
-        gender: ''
-    }
-
     submitted = false;
-    suggestUserName() {
-        const suggestedName = 'Superuser';
-
-        // this is one approach to set a value in the form but it's long and may reset all other changes
-        // this.form.setValue({
-        //     userData: {
-        //         username: suggestedName,
-        //         email: ''
-        //     },
-        //     secret: 'pet',
-        //     questionAnswer: '',
-        //     gender: 'male'
-        // }
-        // )
-
-        
-        // the better approach is to use patchValue because it overrides only parts of the form
-        this.form.form.patchValue({
-            userData: {
-                username: suggestedName
-            }
-        })
+    
+    defaultSubscription = 'advanced';
+    subscribedUser = {
+        email: '',
+        subsciptionType: '',
+        password: ''
     }
-
-    onSubmit(form: NgForm) {
+    onSubmitForm(form: NgForm) {
         this.submitted = true;
-        this.user.username = this.form.value.userData.username;
-        this.user.email = this.form.value.userData.email;
-        this.user.secretQuestion = this.form.value.secret;
-        this.user.answer = this.form.value.questionAnswer;
-        this.user.gender = this.form.value.gender;
+        this.subscribedUser.email = form.value.mail;
+        this.subscribedUser.subsciptionType = form.value.subscriptionType;
+        this.subscribedUser.password = form.value.password;
 
-        // reset the form
-        this.form.reset();
-        this.form.form.controls['secret'].setValue('pet');
+        form.reset();
+        form.form.controls['subscriptionType'].setValue('advanced');
     }
-
-    // alternative option is using the local reference grabbed by ViewChild
-    // onSubmit() {
-    //     console.log(this.form);
-    // }
 }
