70cb7d3b2ca3002b1ff05583ac1f7345faa29ffb
lastminute84 <csabafarkas17@gmail.com>
2018-11-10 21:23:08 +0000

    Using async validators

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -16,7 +17,7 @@ export class AppComponent implements OnInit {
         this.signupForm = new FormGroup({
             'userData': new FormGroup({
                 'username': new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]),  // be careful not executing the validator methods with parentheses
-                'email': new FormControl(null, [Validators.required, Validators.email])
+                'email': new FormControl(null, [Validators.required, Validators.email], this.forbiddenEmails)
             }),
             'gender': new FormControl('male'),
             'hobbies': new FormArray([])
@@ -44,5 +45,22 @@ export class AppComponent implements OnInit {
         }
         // if valid => return null or don't return anything 
         return null;
+    }
+
+    // async validator
+    forbiddenEmails(control: FormControl) : Promise<any> | Observable<any> {
+        const promise = new Promise<any>(
+            (resolve, reject) => {
+                // mimic a server response
+                setTimeout(() => {
+                    if (control.value === 'test@test.com') {
+                        resolve({'emailIsForbidden': true});
+                    } else {
+                        resolve(null);
+                    }
+                }, 1500);
+            }
+        );
+        return promise;
     }
 }
