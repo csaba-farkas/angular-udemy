a7363fb102a13ac82b806d465c6c58dc5f687538
lastminute84 <csabafarkas17@gmail.com>
2018-11-10 21:12:45 +0000

    Creating custom validator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,11 +10,12 @@ export class AppComponent implements OnInit {
     
     genders = ['male', 'female'];
     signupForm: FormGroup;
+    forbiddenUsernames = ['Satan', 'Hitler'];
 
     ngOnInit(): void {
         this.signupForm = new FormGroup({
             'userData': new FormGroup({
-                'username': new FormControl(null, Validators.required), // be careful not executing the validator methods with parentheses
+                'username': new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]),  // be careful not executing the validator methods with parentheses
                 'email': new FormControl(null, [Validators.required, Validators.email])
             }),
             'gender': new FormControl('male'),
@@ -33,5 +34,15 @@ export class AppComponent implements OnInit {
         console.log(this.signupForm.value.username);
         console.log(this.signupForm.value.email);
         console.log(this.signupForm.value.gender);
+    }
+
+    // pay attention to the return type: key -value pairs
+    forbiddenNames(control: FormControl): {[s: string]: boolean} {
+        if (this.forbiddenUsernames.indexOf(control.value) > -1) {
+            // is username contains control.value => return an object with a key value pair
+            return {'nameIsForbidden': true};
+        }
+        // if valid => return null or don't return anything 
+        return null;
     }
 }
